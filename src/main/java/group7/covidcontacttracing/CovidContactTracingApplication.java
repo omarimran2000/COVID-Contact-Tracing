f35d65ee5eaa6cd6627f5407a134dbb5fa4259e3
Omar Imran <51413688+omarimran2000@users.noreply.github.com>
2022-02-20 15:30:24 -0500

    Initial

---
+++ src/main/java/group7/covidcontacttracing/CovidContactTracingApplication.java
@@ -0,0 +1,13 @@
+package group7.covidcontacttracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidContactTracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidContactTracingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group7/covidcontacttracing/CovidContactTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package group7.covidcontacttracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidContactTracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
