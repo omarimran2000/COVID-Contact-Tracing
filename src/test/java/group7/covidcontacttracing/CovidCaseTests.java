3ceeeeea2bb38dee3dbc347324613e10b5c5cd04
BraxtonMartin0 <bqhm1267@gmail.com>
2022-03-10 15:23:26 -0500

    Added reset Spring Boot after each test

--- src/test/java/group7/covidcontacttracing/CovidCaseTests.java
+++ src/test/java/group7/covidcontacttracing/CovidCaseTests.java
@@ -4,17 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 public class CovidCaseTests {
 
     @Autowired
@@ -24,7 +24,7 @@ public class CovidCaseTests {
     public void covidCaseAddTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/covidCaseAdd"))
         .andExpect(status().isOk())
-        .andExpect(content().string(containsString("{\"contactCases\":[],\"id\":4}")));
+        .andExpect(content().string(containsString("{\"contactCases\":[],\"id\":2}")));
 
     }
 
