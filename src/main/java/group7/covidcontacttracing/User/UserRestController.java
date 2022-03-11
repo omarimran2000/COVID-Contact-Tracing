18862542bdfe3f653eaa25cb108e72a7d45b71b2
Omar Imran <51413688+omarimran2000@users.noreply.github.com>
2022-03-11 16:22:42 -0500

    Updated tests

--- src/main/java/group7/covidcontacttracing/User/UserRestController.java
+++ src/main/java/group7/covidcontacttracing/User/UserRestController.java
@@ -5,10 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * User REST Controller Class
+ *
+ * @author Omar Imran
+ * @version March 11 2022
+ */
 @RestController
 public class UserRestController {
     @Autowired
     private UserRepository userRepository ;
+
+    /**
+     * Authenticates users
+     * @param username the user's username
+     * @return if authenticated or not
+     */
     @GetMapping("/checkUser")
     public String checkUser(@RequestParam(value = "username") String username)
     {
---
+++ src/test/java/group7/covidcontacttracing/LoginTests.java
@@ -0,0 +1,34 @@
+package group7.covidcontacttracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+public class LoginTests {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void covidCaseAddTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/checkUser").param("username","omarimran"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("true")));
+
+        mvc.perform(MockMvcRequestBuilders.get("/checkUser").param("username","lebron"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("false")));
+
+    }
+}
