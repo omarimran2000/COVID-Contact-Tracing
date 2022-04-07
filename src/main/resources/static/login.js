711f31b353ff83ac00507a327f7a18f80f274265
BraxtonMartin0 <bqhm1267@gmail.com>
2022-04-07 12:14:53 -0400

    Added password complexity requirements for registering a new user

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -17,12 +17,52 @@ function authenticate() {
     });
 }
 function registerUser(){
-    if ($('#password').val() != $('#password2').val())
-    {
+    const specialCharacters = /[!@#$%^&*()-_=+<>?]/;
+    const uppercase = /[A-Z]/;
+    const lowercase = /[a-z]/;
+    var goodPass = true;
+    $('#errorRegister').hide();
+    $('#errorLength').hide();
+    $('#passHasSpace').hide();
+    $('#passNoCap').hide();
+    $('#passNoLower').hide();
+    $('#passNoSpecial').hide();
 
+    const user = $('#username').val();
+    const pass1 = $('#password').val();
+    const pass2 = $('#password2').val();
+
+    if (pass1 != pass2)
+    {
+        goodPass = false;
         $('#errorRegister').show();
     }
-    else
+    if (pass1.trim().length < 8)
+    {
+        goodPass = false;
+        $('#errorLength').show();
+    }
+    if(pass1.indexOf(' ') >= 0)
+    {
+        goodPass = false;
+        $('#passHasSpace').show();
+    }
+    if(!uppercase.test(pass1))
+    {
+        goodPass = false;
+        $('#passNoCap').show();
+    }
+     if(!lowercase.test(pass1))
+     {
+        goodPass = false;
+        $('#passNoLower').show();
+     }
+    if(!specialCharacters.test(pass1))
+    {
+        goodPass = false;
+        $('#passNoSpecial').show();
+    }
+    if(goodPass)
     {
         $.ajax({
                 url: "/registerUser",
